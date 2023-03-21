9e5e49efa5dac1a45acf60b659f358747ca29fd2
Becky auma <rebeccaauma83@gmail.com>
2023-03-20 18:48:55 -0700

    size of data type

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+char low;
+for (low = 'a'; low <= 'z'; low != q, e;  low++)
+
+putchar(low);
+
+for (low = 'A'; low <= 'Z'; low++)
+
+putchar(low);
+putchar('\n');
+return (0);
+}
