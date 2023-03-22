3035478a7c96caa1c94aaadb1f897e81660f0c10
Becky auma <rebeccaauma83@gmail.com>
2023-03-22 16:58:36 -0700

    letter

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,7 +4,7 @@
  * @c: character to be checked
  * Return: Always 1 if it is a letter and 0 if it is not
  */
-int _isalpha(int c);
+int _isalpha(int c)
 {
 if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 {
