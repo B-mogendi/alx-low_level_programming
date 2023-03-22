189f09f74688bb0605217bcb36d5567a57507483
Becky auma <rebeccaauma83@gmail.com>
2023-03-22 12:21:14 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,19 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
+#include "main.h"
 /**
  * main - entry point
- * Return: Always 0 (success)
- *
+ * _putchar - writes the character c to stdout
+ *Return : Always 0 (success)
  */
 int main(void)
 {
-printf("_putchar\n");
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 return (0);
 }
