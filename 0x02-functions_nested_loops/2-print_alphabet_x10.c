5fde3dde2923819513ce9a74c32ac29916d8dce3
Becky auma <rebeccaauma83@gmail.com>
2023-03-22 14:52:31 -0700

    lowercase

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet_x10  - check the code.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+int i = 1;
+int c;
+do {
+for (c = 'a'; c = 'z'; c++)
+_putchar(c);
+}
+_putchar('\n);
+while (i <= 10)
+return (0);
+}
