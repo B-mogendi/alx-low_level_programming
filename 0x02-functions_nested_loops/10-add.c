e6682f482af901cf251324b0d529e17ce9b15baa
Becky auma <rebeccaauma83@gmail.com>
2023-03-22 17:17:09 -0700

    addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - addition of numbers
+ * @int: numbers added
+ * Return: Always 0.
+ */
+int add(int, int)
+{
+int a, b, c;
+int c = a + b;
+int add(a, b)
+_putchar(c)
+return (0);
+}
+
