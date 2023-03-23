23bb296f34df46fd04420db64fbe5cd0806885f1
Becky auma <rebeccaauma83@gmail.com>
2023-03-23 16:38:33 -0700

    e

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * more_numbers - 1-14, 10 times
+ * Return 0: if success
+ */
+void more_numbers(void)
+{
+int i;
+int g;
+for (i = 1; i <= 14; i++)
+{
+for (g = 0; g = 10;)
+{
+while (g < 10)
+{
+_putchar(i);
+g++;
+}
+}
+}
+_putchar('\n');
+}
