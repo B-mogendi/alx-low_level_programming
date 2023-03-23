b0eb710d580c5aabd722dd2028d6bbd46de73994
Becky auma <rebeccaauma83@gmail.com>
2023-03-23 11:54:16 -0700

    prints numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
  * print_numbers - it outputs numbers
- * Return: 0 if success
+ * Return: ....
  */
 void print_numbers(void)
 {
-int c;
-for (c = 0; c <= 9; c++)
+char c;
+for (c = '0'; c <= '9'; c++)
 {
 _putchar(c);
-_putchar('\n');
 }
+_putchar('\n');
 }
